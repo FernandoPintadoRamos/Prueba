1423dfb560d0e1e336c5b6f2da70c27285bb4306
Fernando Pintado Ramos <fpintadoramos@gmail.com>
2023-02-24 11:54:55 +0100

    telefono

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ const urlParams = new URLSearchParams(valores);
 var direccion = urlParams.get('direccion');
 var cif = urlParams.get('cif');
 var email = urlParams.get('email');
+var telefono = urlParams.get('telefono');
 
 document.getElementById("direccion").innerHTML = direccion;
 document.getElementById("cif").innerHTML = cif;
 document.getElementById("email").innerHTML = email;
+document.getElementById("telefono").innerHTML = telefono;
 
 // FFACUTURAS
 var numero = urlParams.get('numero');
