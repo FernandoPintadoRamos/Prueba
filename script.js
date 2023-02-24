d5651c1dfd2ee4c96112fdf1956655e77ad3ccc8
Fernando Pintado Ramos <fpintadoramos@gmail.com>
2023-02-24 09:25:29 +0100

    Prueba css

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const valores = window.location.search;
-    const urlParams = new URLSearchParams(valores);
-    var nombre = urlParams.get('Nombre');
-    document.getElementById("Nombre").innerHTML = nombre;
+const urlParams = new URLSearchParams(valores);
+
+var nombre = urlParams.get('Nombre');
+
+document.getElementById("Nombre").innerHTML = nombre;
