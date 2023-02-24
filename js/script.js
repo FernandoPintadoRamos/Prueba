61119f4a5d1311de5082fd1da392b57411683376
Fernando Pintado Ramos <fpintadoramos@gmail.com>
2023-02-24 11:30:43 +0100

    Direccion

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const urlParams = new URLSearchParams(valores);
 
 var numero = urlParams.get('numero');
 var fecha = urlParams.get('fecha');
+var direccion = urlParams.get('direccion');
 
 document.getElementById("numero").innerHTML = numero;
 document.getElementById("fecha").innerHTML = fecha;
+document.getElementById("direccion").innerHTML = direccion;
