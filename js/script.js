c13fce22cb9e2991f4dd49edadaf5bec2c429de4
Fernando Pintado Ramos <fpintadoramos@gmail.com>
2023-02-24 11:36:31 +0100

    CIF

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const valores = window.location.search;
 const urlParams = new URLSearchParams(valores);
 
+// FDATOS
+var direccion = urlParams.get('direccion');
+var cif = urlParams.get('cif');
+
+document.getElementById("direccion").innerHTML = direccion;
+document.getElementById("cif").innerHTML = cif;
+
+// FFACUTURAS
 var numero = urlParams.get('numero');
 var fecha = urlParams.get('fecha');
-var direccion = urlParams.get('direccion');
 
 document.getElementById("numero").innerHTML = numero;
 document.getElementById("fecha").innerHTML = fecha;
-document.getElementById("direccion").innerHTML = direccion;
