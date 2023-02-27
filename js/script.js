230def63698e75875053502cbd1b0289376a9e6e
Fernando Pintado Ramos <fpintadoramos@gmail.com>
2023-02-27 09:48:45 +0100

    codigoQR v1.0

--- js/script.js
+++ js/script.js
@@ -26,5 +26,44 @@ document.getElementById("cifConsig").innerHTML = cifConsig;
 var numero = urlParams.get('numero');
 var fecha = urlParams.get('fecha');
 
+
+
+// FDETALLE
+var dets = parseInt(urlParams.get('dets'));
+var imp_total = 0.0;
+
+for(i = 0; i < parseInt(dets); i++){
+    var detalles = document.getElementById("detalles");
+    var p1 = document.createElement("p");
+    var p2 = document.createElement("p");
+    var p3 = document.createElement("p");
+    var p4 = document.createElement("p");
+    var p5 = document.createElement("p");
+    var p6 = document.createElement("p");
+
+    var det_desc = urlParams.get('det_name' + i);
+    var det_imp  = urlParams.get('det_imp'  + i); 
+
+    p1.innerHTML = det_desc;
+    p2.innerHTML = "1";
+    p3.innerHTML = "Unidad";
+    p4.innerHTML = det_imp + " €";
+    p5.innerHTML = "IVA";
+    p6.innerHTML = "Importe €";
+
+    detalles.appendChild(p1);
+    detalles.appendChild(p2);
+    detalles.appendChild(p3);
+    detalles.appendChild(p4);
+    detalles.appendChild(p5);
+    detalles.appendChild(p6);
+
+    imp_total = imp_total + parseFloat(det_imp);
+}
+
+// TOTALES
+document.getElementById("subtotal_sinIVA").innerHTML = imp_total.toFixed(2) + " €";
+
+
 document.getElementById("numero").innerHTML = numero;
 document.getElementById("fecha").innerHTML = fecha;
